3cfb71c401a93ab15584358028d7f48640d4ec03
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-14 16:30:42 +0100

    done

---
+++ src/app.js
@@ -0,0 +1,16 @@
+var h1 = document.querySelector('h1');
+var h2 = document.querySelector('h2');
+
+h1.innerText = coverTitle;
+h2.innerText = coverSubtitle;
+
+var screenHeight = window.innerHeight;
+
+window.onmousemove = function (e) {
+  console.log(e.clientY);
+  var percentage = e.clientY / screenHeight * 100;
+  
+  percentage = Math.min(Math.max(percentage, 8), 95);
+  h1.style.height = percentage + '%';
+  h2.style.height = 100 - percentage + '%';
+};
