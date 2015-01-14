21f3a5f51655aa63d4d0148d6465fda9966066f7
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-14 16:33:52 +0100

    Update 2015-01-14T15:33:49.918Z

--- app.js
+++ app.js
@@ -21,3 +21,18 @@ if (typeof urlVars.subtitle !== 'undefined') {
 } else {
   var coverSubtitle = 'Subtitle was not passed';
 }
+
+var h1 = document.querySelector('h1');
+var h2 = document.querySelector('h2');
+
+h1.innerText = coverTitle;
+h2.innerText = coverSubtitle;
+
+var screenHeight = window.innerHeight;
+
+window.onmousemove = function (e) {
+  var percentage = e.clientY / screenHeight * 100;
+  percentage = Math.min(Math.max(percentage, 8), 95);
+  h1.style.height = percentage + '%';
+  h2.style.height = 100 - percentage + '%';
+};
