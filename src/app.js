5750dbe6a430e88137283191c4de558c2a50820b
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-14 16:31:15 +0100

    done

--- src/app.js
+++ src/app.js
@@ -7,9 +7,7 @@ h2.innerText = coverSubtitle;
 var screenHeight = window.innerHeight;
 
 window.onmousemove = function (e) {
-  console.log(e.clientY);
   var percentage = e.clientY / screenHeight * 100;
-  
   percentage = Math.min(Math.max(percentage, 8), 95);
   h1.style.height = percentage + '%';
   h2.style.height = 100 - percentage + '%';
